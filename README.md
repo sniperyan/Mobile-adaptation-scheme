aa95375020e88350ebe3d3a552ff08600900e99f
e <304787171@qq.com>
2016-12-01 23:44:47 +0800

    add px2rem

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Mobile-adaptation-scheme
-移动端适配解决方案
+移动端适配解决方案,改编于阿里的[flexible.js](https://github.com/amfe/lib-flexible)
 
-#### 利用rem适配移动端页面
-#### 部分安卓机会不支持initial-scale=0.5这种类似的缩放，只支持initial-scale=1，所以需要判断，
-如果是安卓机，就不做高清设备的优化，ios机型，需要做高清缩放适配
+## 主要实现了以下几个功能点
+1. 利用rem适配移动端页面
+1. 部分安卓机会不支持initial-scale=0.5这种类似的缩放，只支持initial-scale=1，所以需要判断，如果是安卓机，就不做高清设备的优化，ios机型，需要做高清缩放适配
+1. 利用`gulp-px2rem`插件将px全部转换成rem单位
--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,23 @@ var rename = require('gulp-rename');                    //重命名
 var sourcemaps = require('gulp-sourcemaps');            //生成 sourcemap 文件方便调试
 var autoprefixer = require('gulp-autoprefixer');        //添加浏览器前缀
 var less = require('gulp-less');                        //编译 Less 文件
+var px2rem = require('gulp-px2rem');                    //px2rem
 /***********************构建source*******************************/
+var px2remOptions = {
+    rootValue: 64,   //640px设计稿
+    unitPrecision: 5,  //保留5位小数
+    propertyBlackList: [],
+    propertyWhiteList: [],
+    replace: true,    //是否用rem替换px
+    mediaQuery: false,
+    minPx: 1
+};
+var postCssOptions = {
+    map: true
+};
 gulp.task('less', function () {
     gulp.src(['./less/**/*.less'])
+        .pipe(px2rem(px2remOptions, postCssOptions))
         .pipe(sourcemaps.init())
         .pipe(less())
         .pipe(autoprefixer({browsers: '> 0%'}))
